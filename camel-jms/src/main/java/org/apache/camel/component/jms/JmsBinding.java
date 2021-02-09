caec1c718c1398a3f1a185b83e16ecf4e2afc607
= <dhilpipre@newrelic.com>
2021-02-09 14:06:26 -0600

    changed deprecated API calls

--- camel-jms/src/main/java/org/apache/camel/component/jms/JmsBinding.java
+++ camel-jms/src/main/java/org/apache/camel/component/jms/JmsBinding.java
@@ -1,19 +1,19 @@
 package org.apache.camel.component.jms;
 
-import com.newrelic.api.agent.DistributedTracePayload;
+import java.util.Map;
+
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.OutboundHeaders;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-import com.nr.instrumentation.camel.jms.CamelJMSUtils;
+import com.nr.instrumentation.camel.jms.JMSHeaders;
 import com.nr.instrumentation.camel.jms.OutboundMsgWrapper;
-import java.util.Map;
-import java.util.logging.Level;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
 
 @Weave
 public abstract class JmsBinding {
@@ -21,71 +21,29 @@ public abstract class JmsBinding {
     Message msg = (Message)Weaver.callOriginal();
     OutboundMsgWrapper wrapper = new OutboundMsgWrapper(msg);
     NewRelic.getAgent().getTracedMethod().addOutboundRequestHeaders((OutboundHeaders)wrapper);
-    if (CamelJMSUtils.distributedTracingEnabled) {
-      DistributedTracePayload dtPayload = NewRelic.getAgent().getTransaction().createDistributedTracePayload();
-      try {
-        String dtText = dtPayload.text();
-        if (dtText != null && 
-          !msg.propertyExists("newrelic"))
-          msg.setStringProperty("newrelic", dtPayload.text()); 
-      } catch (JMSException e) {
-        NewRelic.getAgent().getLogger().log(Level.FINE, (Throwable)e, "Failed to set distributed tracing payload");
-      } 
-    } 
+    JMSHeaders headers = new JMSHeaders(msg);
+    NewRelic.getAgent().getTransaction().insertDistributedTraceHeaders(headers);
     return msg;
   }
   
   protected Message createJmsMessage(Exchange exchange, Object body, Map<String, Object> headers, Session session, CamelContext context) {
     Message msg = (Message)Weaver.callOriginal();
-    OutboundMsgWrapper wrapper = new OutboundMsgWrapper(msg);
-    NewRelic.getAgent().getTracedMethod().addOutboundRequestHeaders((OutboundHeaders)wrapper);
-    if (CamelJMSUtils.distributedTracingEnabled) {
-      DistributedTracePayload dtPayload = NewRelic.getAgent().getTransaction().createDistributedTracePayload();
-      try {
-        String dtText = dtPayload.text();
-        if (dtText != null && 
-          !msg.propertyExists("newrelic"))
-          msg.setStringProperty("newrelic", dtPayload.text()); 
-      } catch (JMSException e) {
-        NewRelic.getAgent().getLogger().log(Level.FINE, (Throwable)e, "Failed to set distributed tracing payload");
-      } 
-    } 
+    JMSHeaders jmsHeaders = new JMSHeaders(msg);
+    NewRelic.getAgent().getTransaction().insertDistributedTraceHeaders(jmsHeaders);
     return msg;
   }
   
   protected Message createJmsMessageForType(Exchange exchange, Object body, Map<String, Object> headers, Session session, CamelContext context, JmsMessageType type) {
     Message msg = (Message)Weaver.callOriginal();
-    OutboundMsgWrapper wrapper = new OutboundMsgWrapper(msg);
-    NewRelic.getAgent().getTracedMethod().addOutboundRequestHeaders((OutboundHeaders)wrapper);
-    if (CamelJMSUtils.distributedTracingEnabled) {
-      DistributedTracePayload dtPayload = NewRelic.getAgent().getTransaction().createDistributedTracePayload();
-      try {
-        String dtText = dtPayload.text();
-        if (dtText != null && 
-          !msg.propertyExists("newrelic"))
-          msg.setStringProperty("newrelic", dtPayload.text()); 
-      } catch (JMSException e) {
-        NewRelic.getAgent().getLogger().log(Level.FINE, (Throwable)e, "Failed to set distributed tracing payload");
-      } 
-    } 
+    JMSHeaders jmsHeaders = new JMSHeaders(msg);
+    NewRelic.getAgent().getTransaction().insertDistributedTraceHeaders(jmsHeaders);
     return msg;
   }
   
   public Message makeJmsMessage(Exchange exchange, org.apache.camel.Message camelMessage, Session session, Exception cause) {
     Message msg = (Message)Weaver.callOriginal();
-    OutboundMsgWrapper wrapper = new OutboundMsgWrapper(msg);
-    NewRelic.getAgent().getTracedMethod().addOutboundRequestHeaders((OutboundHeaders)wrapper);
-    if (CamelJMSUtils.distributedTracingEnabled) {
-      DistributedTracePayload dtPayload = NewRelic.getAgent().getTransaction().createDistributedTracePayload();
-      try {
-        String dtText = dtPayload.text();
-        if (dtText != null && 
-          !msg.propertyExists("newrelic"))
-          msg.setStringProperty("newrelic", dtPayload.text()); 
-      } catch (JMSException e) {
-        NewRelic.getAgent().getLogger().log(Level.FINE, (Throwable)e, "Failed to set distributed tracing payload");
-      } 
-    } 
+    JMSHeaders jmsHeaders = new JMSHeaders(msg);
+    NewRelic.getAgent().getTransaction().insertDistributedTraceHeaders(jmsHeaders);
     return msg;
   }
 }
